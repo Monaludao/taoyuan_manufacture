aeba7a10b414fa8184c361a6b6971d722ae8bf83
Richard Hsieh <monaludao@gmail.com>
2016-06-14 17:09:53 +0800

    mapper version 1

--- ty_industry_parse.R
+++ ty_industry_parse.R
@@ -11,17 +11,40 @@ industry_parse <- function() {
   library(ggplot2)
   library(ggmap)
   map.df <- data.df[!is.na(data.df$Response_X),]
-  map.df <- map.df[grepl("機械設備",map.df$產業類別),]
   
-  mapgilbert <- get_map(location = c(lon = median(map.df$Response_X)-0.05, lat = median(map.df$Response_Y)), zoom = 11,
+  map.lon <- median(map.df$Response_X)-0.05
+  map.lat <- median(map.df$Response_Y)
+
+  map.cat <- c("全部","金屬","電子","機械","化學","紡織","食品")
+  
+  mapgilbert <- get_map(location = c(lon = map.lon, lat = map.lat), zoom = 11,
                         maptype = "toner-lite", scale = 1, language="zh-TW")
-  ggmap(mapgilbert) +
-    geom_point(data = map.df, aes(x = Response_X, y = Response_Y, fill = "red", alpha = 0.8), size = 1, shape = 21) +
-    guides(fill=FALSE, alpha=FALSE, size=FALSE)
   
-    #stat_density2d(aes(x = map.df$Response_X, y = map.df$Response_Y, fill = ..level.., alpha = ..level..),
-                   #size = 2, bins = 4, data = map.df$工廠登記工廠設立核准日期, geom = "polygon") +
-    #scale_fill_gradient("Violent\nCrime\nDensity")
     
-    
+  for (i in 1:length(map.cat)){
+    if (i != 1){
+      map.cat.df <- map.df[grepl(map.cat[i],map.df$產業類別),]
+    } else {
+      map.cat.df <- map.df
+    }
+    mapping(i,map.cat[i],map.cat.df,mapgilbert)
+  }
+}
+
+mapping <- function(i,map.cat,map.df,mapgilbert) {
+  if(i == 1){
+    g<- ggmap(mapgilbert) +
+        coord_cartesian() +
+        geom_point(data = map.df, aes(x = Response_X, y = Response_Y, colour="red",alpha = 0.01), size = .5) +
+        guides(fill=FALSE, colour=FALSE, alpha=FALSE, size=FALSE)
+    print(paste0("exporting 00",map.cat,"點位.png"))
+    ggsave(g,filename=paste0("00",map.cat,"點位.png"),width=8,height=8)
+  }
+  
+  g<- ggmap(mapgilbert) +
+      coord_cartesian() +
+      stat_binhex(data = map.df,aes(x = Response_X, y = Response_Y, alpha =.05), binwidth=c(0.006, 0.006)) +
+      theme(legend.position=c(.96,.1)) + scale_alpha(guide="none") + scale_fill_gradient(low="azure2",high="red4")
+  print(paste0("exporting 0",i,map.cat,"相關.png"))
+  ggsave(g,filename=paste0("0",i,map.cat,"相關.png"),width=8,height=8)
 }
